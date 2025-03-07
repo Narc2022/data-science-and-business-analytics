f2e3d86907e81bcf741e8c80c73c0b8712c0c511
sachin <sachinnarofficials@gmail.com>
2025-03-07 07:38:05 +0530

    logocal  oprator

--- python/p_logical_operation.py
+++ python/p_logical_operation.py
@@ -5,15 +5,17 @@ student = True
 if high_income and good_credit:
     print("Eligible")
 else:
-    print("Not Eligible") 
+    print("Not Eligible")
 # use of or operator
 
-if not student :
+if not student:
     print("Eligible")
 else:
     print("Not Eligible")
 
 # use of or and and operator
 
-if(high_income or good_credit) and not student:
+if (high_income or good_credit) and not student:
     print("Eligible")
+else:
+    print("Not Eligible")
