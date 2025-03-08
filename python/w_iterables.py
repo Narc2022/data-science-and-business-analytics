200a6912fb100f425c47e0be1dc729e982fab8de
sachin <sachinnarofficials@gmail.com>
2025-03-08 18:48:26 +0530

    funtion

---
+++ python/w_iterables.py
@@ -0,0 +1,19 @@
+for x in "Python":
+    print(x)
+# output
+# P
+# y
+# t
+# h
+# o
+# n
+
+for x in [1, 2, 3, 4]:
+    print(x)
+
+# output
+
+# 1
+# 2
+# 3
+# 4
---
+++ python/z_function.py
@@ -0,0 +1,15 @@
+def greet():
+    print("Hi there")
+    print("Welcome abroad")
+
+
+greet()
+
+
+# greet function with args
+def greet_args(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("Welcome abroad")
+
+
+greet_args("Sachin", "Singh")
